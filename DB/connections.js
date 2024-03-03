21c6ee3c5f87a8ee23fb8390f31f9bcd9ec39d76
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-03 19:22:36 +0300

    COMMIT

--- DB/connections.js
+++ DB/connections.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://cse341-api:7Mwathani77@cluster0.u9kddnv.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const URI = "mongodb+srv://cse341-api:7Mwathani77@cluster0.u9kddnv.mongodb.net/";
+
+const connectDB = async()=>{
+    await mongoose.connect(URI);
+}
+
+module.exports = connectDB;
