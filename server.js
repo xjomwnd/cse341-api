521d9d75fa78a8ef7a0b84cd2afa6905c2d73c4f
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-03 20:31:04 +0300

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost:27017/myDatabase';
+const connectionString = 'mongodb://localhost:27017/mongodb+srv://cse341-api:7Mwathani77@cluster0.u9kddnv.mongodb.net/';
+
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
