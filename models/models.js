4bade038a5fbe1b95449adbe3c434c8b001aa264
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-03 22:06:49 +0300

    commit

---
+++ models/models.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  age: {
+    type: Number,
+    required: false
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
