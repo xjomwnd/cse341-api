81bfb1829b3f3ef7158ac4d827d5fb8d0494c7a1
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-03 19:28:21 +0300

    commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDB = require('./connections');
 const app = express();
+
+connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
