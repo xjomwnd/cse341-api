a0ed62b6d6edd12b2e94bc3cfb6376e32f7cf8a5
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-03 19:36:04 +0300

    COMMIT

--- DB/connections.js
+++ DB/connections.js
@@ -4,6 +4,7 @@ const URI = "mongodb+srv://cse341-api:7Mwathani77@cluster0.u9kddnv.mongodb.net/"
 
 const connectDB = async()=>{
     await mongoose.connect(URI);
+    console.log('db connected..!');
 }
 
 module.exports = connectDB;
