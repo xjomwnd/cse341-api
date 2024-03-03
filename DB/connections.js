69d0f2e87d188b386030e7903abe51ecb9c7c043
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-03 18:45:00 +0300

    commit

---
+++ DB/connections.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('your_mongodb_uri', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
